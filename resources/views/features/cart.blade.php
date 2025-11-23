c22fc8176c1830051396ea6522adb2e58690a848
ZARTH24 <114113937+ZARTH24@users.noreply.github.com>
2025-11-23 20:30:43 +0800

    Uodate views + route

---
+++ resources/views/features/cart.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h3>Your Cart</h3>
+<p>Your cart is empty.</p>
+@endsection
---
+++ resources/views/features/profile.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h3>User Profile</h3>
+<p>Profile details here...</p>
+@endsection
